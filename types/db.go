0460fcbf3cf9916d0e265c2040ccf2ade118890e
Sean Gugler <sean.gugler@gmail.com>
2024-01-15 19:21:27 -0800

    remove unused code from db module

--- types/db.go
+++ types/db.go
@@ -1,19 +1,10 @@
 package types
 
 import (
-	// "database/sql"
-	// "log"
-	// "os"
 	"strings"
 
-	// "github.com/pkg/errors"
 	"github.com/xeals/signal-back/signal"
-	_ "modernc.org/sqlite"
 )
-
-// type SqlParameter struct {
-	// signal.SqlStatement_SqlParameter
-// }
 
 type ColumnType int
 const (
@@ -47,7 +38,6 @@ func NewSchema(statement_params string) *Schema {
 		Index: make(map[string]int),
 		Type: make([]ColumnType, len(cols)),
 	}
-	// log.Println(s)
 
 	// Directives like "UNIQUE(field, field)" get split by commas, too.
 	// Handle this by skipping opening through closing parentheses.
@@ -72,15 +62,11 @@ func NewSchema(statement_params string) *Schema {
 			}
 			continue
 		}
-		// log.Printf("name `%s`", name)
 
 		// Map column names to their index number
 		s.Index[name] = i - j
-		// log.Printf("index %d", s.Index[name])
 
 		if len(parts) > 1 {
-			// log.Printf("type `%s`", parts[1])
-			// log.Printf("coltype %d", columnTypeFromString(parts[1]))
 			s.Type[i] = columnTypeFromString(parts[1])
 		}
 	}
@@ -88,28 +74,18 @@ func NewSchema(statement_params string) *Schema {
 }
 
 func (s *Schema) Field (row []*signal.SqlStatement_SqlParameter, column string) interface{} {
-// func (s *Schema) Field (row []*SqlParameter, column string) interface{} {
 	i := s.Index[column]
 	t := s.Type[i]
 	return ParameterValue(row[i], t)
-	// return row[i].Value(t)
 }
 
-// var ptr *T
-// ptr = Field(schema, params, "mid").(*T)
-
 func (s *Schema) RowValues (row []*signal.SqlStatement_SqlParameter) []interface{} {
-// func (s *Schema) RowValues (row []*SqlParameter, column string) []interface{} {
 	pv := make([]interface{}, len(row))
 	for i, v := range row {
 		pv[i] = ParameterValue(v, s.Type[i])
-		// pv[i] = v.Value(s.Type[i])
 	}
-	// log.Println(pv)
 	return pv
 }
-
-// db.Exec(stmt, PV...)
 
 // Backups.proto declares IntegerParameter as uint64
 //   but there are values of -1 in the database.
@@ -125,7 +101,6 @@ func signed(u *uint64) *int64 {
 }
 
 func ParameterValue (p *signal.SqlStatement_SqlParameter, typ ColumnType) interface{} {
-// func (p *SqlParameter) Value (typ ColumnType) interface{} {
 	// https://www.sqlite.org/datatype3.html#type_affinity
 	//     "The type affinity of a column is the recommended type for data stored
 	//     in that column. The important idea here is that the type is recommended,
